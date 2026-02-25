32d63609682b7ffd0cd98a95b1b6eb4d6833670e
udon <yukisfuture@gmail.com>
2026-02-25 14:13:45 +0900

    Rename examplemod to watatsumi and successfully load mod

--- src/main/java/com/udon/watatsumi/WatatsumiClient.java
+++ src/main/java/com/udon/watatsumi/WatatsumiClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.udon.watatsumi;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,11 +11,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = Watatsumi.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
-public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+@EventBusSubscriber(modid = Watatsumi.MODID, value = Dist.CLIENT)
+public class WatatsumiClient {
+    public WatatsumiClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -25,7 +25,7 @@ public class ExampleModClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        Watatsumi.LOGGER.info("HELLO FROM CLIENT SETUP");
+        Watatsumi.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
