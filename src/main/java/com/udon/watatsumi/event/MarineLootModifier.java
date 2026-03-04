982f0de7ea20e513725ffbbb8eab73faaa38aa17
udon <yukisfuture@gmail.com>
2026-03-04 11:57:08 +0900

    refactor: restore marine tag based loot logic

--- src/main/java/com/udon/watatsumi/event/MarineLootModifier.java
+++ src/main/java/com/udon/watatsumi/event/MarineLootModifier.java
@@ -6,6 +6,9 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.entity.LivingEntity;
+import com.udon.watatsumi.registry.ModTags;
 
 import com.mojang.serialization.MapCodec;
 
@@ -15,8 +18,19 @@ public record MarineLootModifier() implements IGlobalLootModifier {
             MapCodec.unit(new MarineLootModifier());
 
     @Override
-    public ObjectArrayList<ItemStack> apply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(new ItemStack(ModItems.TAMATEBAKO.get()));
+    public ObjectArrayList<ItemStack> apply(ObjectArrayList<ItemStack> generatedLoot,
+                                            LootContext context) {
+
+        var entity = context.getParamOrNull(LootContextParams.THIS_ENTITY);
+
+        if (entity instanceof LivingEntity living &&
+                living.getType().is(ModTags.EntityTypes.MARINE)) {
+
+            System.out.println("MarineLootModifier fired (tag check)");
+
+            generatedLoot.add(new ItemStack(ModItems.TAMATEBAKO.get()));
+        }
+
         return generatedLoot;
     }
     @Override
